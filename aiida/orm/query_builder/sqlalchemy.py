b07745afded509bf6a06d5520be6797427e5e036
Jocelyn Boullier <jocelyn.boullier@epfl.ch>
2015-05-15 22:56:38 +0200

    Remove some unnecessary join. I hope with some perf update.

--- aiida/orm/query_builder/sqlalchemy.py
+++ aiida/orm/query_builder/sqlalchemy.py
@@ -3,10 +3,9 @@ from __future__ import absolute_import
 
 __all__ = ['QueryBuilder']
 
-
 from aiida.djsite.db.models import DbNode, DbAttribute, DbUser, DbExtra, \
-    DbGroup, DbPath
-from sqlalchemy import and_, or_
+    DbGroup, DbPath, DbLink
+from sqlalchemy import and_, or_, select
 from sqlalchemy.orm.query import aliased
 from functools import partial
 
@@ -18,6 +17,7 @@ User = DbUser.sa
 Extra = DbExtra.sa
 Group = DbGroup.sa
 Path = DbPath.sa
+Link = DbLink.sa
 
 string_to_op = {
     "==": lambda e: e.__eq__,
@@ -49,6 +49,8 @@ class QueryBuilder(object):
     def __init__(self):
         self.alias = {"input": aliased(Node, name="input"),
                       "output": aliased(Node, name="output"),
+                      "input_link": aliased(Link, name="input_link"),
+                      "output_link": aliased(Link, name="output_link")
                       }
 
         self.filters = []
@@ -58,12 +60,16 @@ class QueryBuilder(object):
         self.attr_to_prefetch = []
         self.extra_to_prefetch = []
 
+
+        # Filters used when chaining query.
         self.input_filters = []
         self.output_filters = []
 
+        # Filters used to filter the attribures of the relation
         self.input_attr_filters = []
         self.output_attr_filters = []
 
+        # Attributes to prefetch.
         self.input_attr_to_prefetch = []
         self.output_attr_to_prefetch = []
 
@@ -78,17 +84,13 @@ class QueryBuilder(object):
         q = self._build_query()
         return q.all()
 
-    def _build_relation_stmt(self, relation, filters):
-        table = self.alias[relation]
-        attr_filter = map(
-            lambda a: Attribute.query()
-            .filter(a, table.id == Attribute.dbnode_id).exists()
-            .correlate(table),
-            filters)
+    def _build_relation_stmt(self, relation_column, filters):
 
-        sub_query = table.query(table.id).filter(*attr_filter).subquery()
+        sub_query = and_(*map(
+            lambda a: relation_column.in_(select([Attribute.dbnode_id]).where(a)),
+        filters))
 
-        return table.id.in_(sub_query)
+        return sub_query
 
     def _build_depth_filter(self, relation,  filters):
         # Unpack (filters, min_depth, max_depth)
@@ -116,13 +118,13 @@ class QueryBuilder(object):
 
         return filter
 
-    def _build_prefetch(self, attrs, relation=None):
+    def _build_prefetch(self, attrs, relation_column=None):
+        # A bit hackish to use relation_column to properly do the join.
         alias, join = (Attribute, Attribute)
 
-        if relation:
+        if relation_column:
             alias = aliased(Attribute)
-            table = self.alias[relation]
-            join = (alias, alias.dbnode_id == table.id)
+            join = (alias, alias.dbnode_id == relation_column)
 
         prefetch_keys = alias.key.in_(map(lambda a: a.key, attrs))
         prefetch_columns = (alias.key,) + tuple(
@@ -130,28 +132,70 @@ class QueryBuilder(object):
                 map(lambda a: getattr(alias, a.column),
                     attrs)
             ))
-        if relation:
-            prefetch_columns = map(lambda c: c.label(relation + "_" + c.key),
-                                   prefetch_columns)
-            self.columns_names += map(lambda c: relation + "_" + c.key,
-                                      prefetch_columns)
-        else:
-            self.columns_names += map(lambda c: c.key, prefetch_columns)
+        # No need to maintain this for now.
+        # if relation:
+        #     prefetch_columns = map(lambda c: c.label(relation + "_" + c.key),
+        #                            prefetch_columns)
+        #     self.columns_names += map(lambda c: relation + "_" + c.key,
+        #                               prefetch_columns)
+        # else:
+        #     self.columns_names += map(lambda c: c.key, prefetch_columns)
 
         return (join, prefetch_keys, prefetch_columns)
+
+    def _build_input_query(self, q):
+        if len(self.input_attr_to_prefetch + self.input_filters +
+               self.input_attr_filters) == 0:
+            return q
+
+        table = self.alias["input_link"]
+
+        q = q.join(table, Node.input_links)
+
+        if len(self.input_attr_filters) > 0:
+            stmt = self._build_relation_stmt(table.input_id,
+                                             self.input_attr_filters)
+            q = q.filter(stmt)
+        if len(self.input_filters) > 0:
+            q = q.filter(*self.input_filters)
+
+        if len(self.input_attr_to_prefetch) > 0:
+            join, prefetch_keys, prefetch_columns = self._build_prefetch(
+                self.input_attr_to_prefetch, relation_column=table.input_id)
+            q = q.join(join)
+            q = q.filter(prefetch_keys)
+            q = q.add_columns(*prefetch_columns)
+
+        return q
+
+    def _build_output_query(self, q):
+        if len(self.output_attr_to_prefetch + self.output_filters +
+               self.output_attr_filters) == 0:
+            return q
+
+        table = self.alias["output_link"]
+
+        q = q.join(table, Node.output_links)
+
+        if len(self.output_attr_filters) > 0:
+            stmt = self._build_relation_stmt(table.output_id,
+                                             self.output_attr_filters)
+            q = q.filter(stmt)
+        if len(self.output_filters) > 0:
+            q = q.filter(*self.output_filters)
+
+        if len(self.output_attr_to_prefetch) > 0:
+            join, prefetch_keys, prefetch_columns = self._build_prefetch(
+                self.output_attr_to_prefetch, relation_column=table.output_id)
+            q = q.join(join)
+            q = q.filter(prefetch_keys)
+            q = q.add_columns(*prefetch_columns)
+
+        return q
 
     def _build_query(self):
 
         q = Node.query()
-
-        # TODO: better than a triple length check.
-        if len(self.input_attr_filters) > 0 or len(self.input_filters) > 0 or \
-                len(self.input_attr_to_prefetch) > 0:
-            # needed to form a right join at the beginning, because you can't
-            # do
-            # select_form on an already filtered/join query.
-            input = self.alias["input"]
-            q = q.select_from(input).join(input.outputs)
 
         if len(self.group_filters) > 0:
             q = q.join(Group).filters(Group.name.in_(self.group_filters))
@@ -175,25 +219,8 @@ class QueryBuilder(object):
             q = q.filter(prefetch_keys)
             q = q.add_columns(*prefetch_columns)
 
-        if len(self.input_filters) > 0:
-            q = q.filter(*self.input_filters)
-
-        if len(self.input_attr_filters) > 0:
-            stmt = self._build_relation_stmt("input",
-                                             self.input_attr_filters)
-            q = q.filter(stmt)
-
-        if len(self.output_attr_filters) > 0 or len(self.output_filters) > 0 or \
-            len(self.output_attr_to_prefetch) > 0:
-            q = q.join(self.alias["output"], Node.outputs)
-
-        if len(self.output_filters) > 0:
-            q = q.filter(*self.output_filters)
-
-        if len(self.output_attr_filters) > 0:
-            stmt = self._build_relation_stmt("output",
-                                             self.output_attr_filters)
-            q = q.filter(stmt)
+        q = self._build_input_query(q)
+        q = self._build_output_query(q)
 
         if len(self.children_attr_filters) > 0:
             stmt = and_(
@@ -208,23 +235,6 @@ class QueryBuilder(object):
                     self.parents_attr_filters)
             )
             q = q.filter(stmt)
-
-        # Prefetch using a left outer join. If an attribute doesn't exist, then
-        # ¯\_(ツ)_/¯
-
-        if len(self.input_attr_to_prefetch) > 0:
-            join, prefetch_keys, prefetch_columns = self._build_prefetch(
-                self.input_attr_to_prefetch, relation="input")
-            q = q.join(join)
-            q = q.filter(prefetch_keys)
-            q = q.add_columns(*prefetch_columns)
-
-        if len(self.output_attr_to_prefetch) > 0:
-            join, prefetch_keys, prefetch_columns = self._build_prefetch(
-                self.output_attr_to_prefetch, relation="output")
-            q = q.join(join)
-            q = q.filter(prefetch_keys)
-            q = q.add_columns(*prefetch_columns)
 
         return q
 
@@ -319,12 +329,15 @@ class QueryBuilder(object):
                         min_depth=None, max_depth=None):
         # TODO: proper error message.
         if relation in ("input", "output"):
-            _table = self.alias[relation]
             _filters = None
             if relation == "input":
                 _filters = self.input_filters
+                relation = "input_link"
             elif relation == "output":
                 _filters = self.output_filters
+                relation = "output_link"
+
+            _table = self.alias[relation]
 
             stmt = _table.id.in_(query.with_entities(Node.id))
             _filters.append(stmt)
@@ -341,13 +354,10 @@ class QueryBuilder(object):
             _filters.append((stmt, min_depth, max_depth))
 
         else:
-            raise ValueError("The relation argument needs to be either 'input'," +
-                             "'output', 'children' or 'parents', but was " +
+            raise ValueError("The relation argument needs to be either 'input',"
+                             + "'output', 'children' or 'parents', but was " +
                              "{}".format(relation))
 
-
-
-    # TODO: replace extra to use the table arg
     def _attr_filter_stmt(self, _filter, extra=False):
         table = Extra if extra else Attribute
         stmt = table.key == _filter.key
